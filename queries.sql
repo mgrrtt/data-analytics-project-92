c8a4bbd776979a89fe2e8a1e73e59f3874dc3ec0
Margarita <79995284+mgrrtt@users.noreply.github.com>
2024-01-09 20:57:45 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -51,3 +51,49 @@ INNER JOIN products
 GROUP BY TO_CHAR(sales.sale_date, 'ID'), 1, 2
 ORDER BY TO_CHAR(sales.sale_date, 'ID'), name;
 
+
+-- Количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
+SELECT
+  CASE
+    WHEN age BETWEEN 16 AND 25 THEN '16-25'
+    WHEN age BETWEEN 26 AND 40 THEN '26-40'
+  ELSE '40+'
+  END AS age_category,
+  COUNT(customer_id) AS count
+FROM customers
+GROUP BY 1
+ORDER BY 1;
+
+
+-- Количество уникальных покупателей и выручка, которую они принесли, по месяцам
+SELECT
+  TO_CHAR(sales.sale_date, 'YYYY-MM') AS date,
+  COUNT(DISTINCT customers.customer_id) AS total_customers,
+  ROUND(SUM(products.price * sales.quantity), 0) AS income
+FROM sales
+INNER JOIN customers
+  ON sales.customer_id = customers.customer_id
+INNER JOIN products
+  ON sales.product_id = products.product_id
+GROUP BY 1
+ORDER BY 1;
+
+
+-- Покупатели, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0)
+SELECT DISTINCT ON (CONCAT(customers.first_name, ' ', customers.last_name))
+  CONCAT(customers.first_name, ' ', customers.last_name) AS customer,
+  sales.sale_date,
+  CONCAT(employees.first_name, ' ', employees.last_name) AS seller
+FROM sales
+INNER JOIN customers
+  ON sales.customer_id = customers.customer_id
+INNER JOIN employees
+  ON employees.employee_id = sales.sales_person_id
+INNER JOIN products
+  ON sales.product_id = products.product_id
+WHERE products.price = 0
+ORDER BY
+  CONCAT(customers.first_name, ' ', customers.last_name),
+  customers.customer_id,
+  sales.sale_date;
+
